7f78c11d7a67b6860e970ed7098e8016e01e4aa9
tighzanour <tighzanour@gmail.com>
2025-09-19 13:21:37 -0400

    bug deplacement corrigé

--- player.gd
+++ player.gd
@@ -9,15 +9,16 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
+
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed(&"move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed(&"move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed(&"move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed(&"move_up"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
@@ -25,25 +26,29 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-		
+
 		position += velocity * delta
 		position = position.clamp(Vector2.ZERO, screen_size)
+
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = 'walk'
+		$AnimatedSprite2D.animation = &'right'
 		$AnimatedSprite2D.flip_v = false
+		
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = 'up'
-		$AnimatedSprite2D.flip_v = velocity.y > 0
-		
-
-func _on_body_entered(_body):
-	hide()
-	hit.emit()
-	$CollisionShape2D.set_deferred('disabled', true)
+		$AnimatedSprite2D.animation = &'up'
+		rotation = PI if velocity.y > 0 else 0
 
 
 func start(pos):
 	position = pos
+	rotation = 0
 	show()
 	$CollisionShape2D.disabled = false
+
+
+func _on_body_entered(_body):
+	hide()
+	hit.emit()
+	
+	$CollisionShape2D.set_deferred(&'disabled', true)
